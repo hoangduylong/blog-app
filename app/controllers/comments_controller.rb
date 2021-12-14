addb7c872d78814a05248ef964bfde7cc67ebbd7
nguyeenthianhhoong <hongganhh25@gmail.com>
2021-12-14 19:18:32 +0000

    update comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
+    def new
+        @comment = Comment.new
+    end
     
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(params[:comment].permit(:name, :comment))
-        redirect_to post_path(@post)
+        @comment = Comment.new
+        @comment = @post.comments.create(params[:comment].permit(:user_id,:name, :comment ))
+        @comment.user_id = current_user.id
+        if @comment.save
+            redirect_to post_path(@post)
+        end
+        
     end
     
     
